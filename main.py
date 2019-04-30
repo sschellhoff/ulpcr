d40c971006eca4db0c93fb98a57b82894e3a906f
Tobbe <tobias.naumann@iml.fraunhofer.de>
2019-04-30 14:57:02 +0200

    Make image-receiver stoppable by encapsulating infinite loop into a stoppable thread.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import time
 import matplotlib.pyplot as plt
 import numpy
 
+WIDTH = 320
+HEIGHT = 240
+
 sample_data = bytes([255, 255, 100, 200, 100, 200, 50, 50, 50, 50, 50, 50, 10, 80, 10, 80, 50, 90, 200, 200, 200, 200, 200, 200,
                      100, 200, 100, 200, 100, 200, 50, 50, 50, 50, 50, 50, 10, 80, 10, 80, 50, 90, 200, 200, 200, 200, 200, 200,
                      100, 200, 100, 200, 100, 200, 50, 50, 50, 50, 50, 50, 10, 80, 10, 80, 50, 90, 200, 200, 200, 200, 200, 200,
@@ -18,44 +21,82 @@ sample_data = bytes([255, 255, 100, 200, 100, 200, 50, 50, 50, 50, 50, 50, 10, 8
                      100, 200, 100, 200, 100, 200, 50, 50, 50, 50, 50, 50, 10, 80, 10, 80, 50, 90, 200, 200, 200, 200, 200, 200])
 
 def main():
+    with serial.Serial('COM44', 115200) as serial_connection:
+        receiver = ImageReceiver(serial_connection, WIDTH, HEIGHT)
+        input("Type any key to stop the program")
+        receiver.stop()
+        receiver.join()
+"""
     while True:
-        #image = get_image_from_serial_connection('/dev/ttyUSB0', 115200, 60, 153600, 320)
-        image = get_image_from_serial_connection('COM44', 115200, 60, 153600, 320)
-        #image = create_image_from_binary(sample_data, 12)
-        # Use one of the following two options
-        # a) Show with matplotlib which can display inline graphics in ipython or has a qt frontend
-        plt.imshow(numpy.asarray(image))
-        plt.show()
-        print()
-        # b) Use the default Pillow approach to call an external application
-        #image.show()
+        with serial.Serial('COM44', 115200) as serial_connection:
+            data = get_data_from_serial_connection(serial_connection, WIDTH*HEIGHT*2)
+            image = create_image_from_binary(data, WIDTH)
+            #image = create_image_from_binary(sample_data, 12)
+            # Use one of the following two options
+            # a) Show with matplotlib which can display inline graphics in ipython or has a qt frontend
+            plt.imshow(numpy.asarray(image))
+            plt.show()
+            print()
+            # b) Use the default Pillow approach to call an external application
+            #image.show()
+            """
 
-def get_image_from_serial_connection(device, baudrate, timeout, data_size, image_width):
-    data = get_data_from_serial_connection(device, baudrate, timeout, data_size)
-    image = create_image_from_binary(data, image_width)
-    return image
+# Inspired by (taken from) https://stackoverflow.com/a/325528/805673
+import threading
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
 
-def get_data_from_serial_connection(device, baudrate, timeout, size):
+    def __init__(self):
+        super(StoppableThread, self).__init__()
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+class ImageReceiver(StoppableThread):
+    def __init__(self, serial_connection, width, height):
+        super().__init__()
+        self.serial_connection = serial_connection
+        self.width = width
+        self.height = height
+        self.start() # Thread.start()
+
+    def run(self):
+        while not self.stopped():
+            data = get_data_from_serial_connection(self.serial_connection, self.width*self.height*2, self)
+            image = create_image_from_binary(data, self.width)
+            #image = create_image_from_binary(sample_data, 12)
+            # Use one of the following two options
+            # a) Show with matplotlib which can display inline graphics in ipython or has a qt frontend
+            plt.imshow(numpy.asarray(image))
+            plt.show()
+            print()
+            # b) Use the default Pillow approach to call an external application
+            #image.show()
+
+def get_data_from_serial_connection(serial_connection, size, stoppable_thread=None):
     # Not very nice, but working: We read the whole image byte by byte and reset
     # the image input whenever we encounter a break of more than .5 seconds
     # between two input bytes. As the uC is making breaks between two images,
     # this synchronizes image reception.
     outbuffer = bytearray(size)
-    with serial.Serial(device, baudrate, timeout=timeout) as serial_connection:
-        bytecnt = 0
-        last_timestamp = 0
-        while bytecnt < size: # this could end up in an infinite loop if misconfigured
-            current_byte = serial_connection.read(1)
-            current_timestamp = time.time()
-            if current_timestamp - last_timestamp > .500: # if there was a break, reset image
-                bytecnt = 0
-            last_timestamp = current_timestamp
-            outbuffer[bytecnt] = current_byte[0]
-            bytecnt += 1
-            if bytecnt % 1000 == 0: # For debugging: Show the current progress
-                print('\r            \r', round(bytecnt / size * 100), '%', end='')
-        #data = serial_connection.read(size)
-        data = bytes(outbuffer)
+    bytecnt = 0
+    last_timestamp = 0
+    while bytecnt < size and (stoppable_thread == None or not stoppable_thread.stopped()): # this could end up in an infinite loop if misconfigured
+        current_byte = serial_connection.read(1)
+        current_timestamp = time.time()
+        if current_timestamp - last_timestamp > .500: # if there was a break, reset image
+            bytecnt = 0
+        last_timestamp = current_timestamp
+        outbuffer[bytecnt] = current_byte[0]
+        bytecnt += 1
+        if bytecnt % 1000 == 0: # For debugging: Show the current progress
+            print('\r            \r', round(bytecnt / size * 100), '%', end='')
+    data = bytes(outbuffer)
     return data
 
 def create_image_from_binary(data, width):
